ee4df558c28f17f694e3abc98efe308aa3353788
Václav Oborník <vaclav.obornik@gmail.com>
2015-05-01 12:47:42 +0200

    remove redundant index.d.ts definition

--- di-ts.d.ts
+++ di-ts.d.ts
@@ -1,9 +1,12 @@
 
-/// <reference path="index.d.ts"/>
+/// <reference path="di.d.ts" />
 
 declare module "di-ts" {
 
   export var Injector: typeof di.Injector;
+
   export function Inject(classFunc: any): void;
+
   export function Provide(targetClassFunc: any): (classFunc: any) => void;
+
 }
--- index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="di.d.ts" />
-import * as di from 'di';
-export declare var Injector: typeof di.Injector;
-export declare function Inject(classFunc: any): void;
-export declare function Provide(targetClassFunc: any): (classFunc: any) => void;
