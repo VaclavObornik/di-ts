b7548838658485a835d1b3080d01a079d050d9d2
Eddy Lane <eddy@uvd.co.uk>
2015-12-04 11:56:42 +0000

    Updated compiled index.js file

---
+++ index.js
@@ -0,0 +1,21 @@
+require('reflect-metadata');
+var di = require('di');
+exports.Injector = di.Injector;
+function factory(data) {
+    function F() {
+        di.Inject.apply(this, data);
+    }
+    F.prototype = di.Inject.prototype;
+    return new F();
+}
+function Inject(classFunc) {
+    var dependencies = Reflect.getMetadata("design:paramtypes", classFunc) || [];
+    di.annotate(classFunc, factory(dependencies));
+}
+exports.Inject = Inject;
+function Provide(targetClassFunc) {
+    return function (classFunc) {
+        di.annotate(classFunc, new di.Provide(targetClassFunc));
+    };
+}
+exports.Provide = Provide;
